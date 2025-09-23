69dc74e252653a989a0ba64deecde1cf330f4218
Dante <dantelrharrell@gmail.com>
2025-09-22 23:21:51 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
-cat > start.sh <<'SH'
 #!/bin/bash
-PORT_NUMBER=${PORT:-5000}
-exec gunicorn main:app --bind 0.0.0.0:$PORT_NUMBER --workers 3
-SH
-
-chmod +x start.sh
+echo "PWD: $(pwd)"
+echo "Listing files:"
+ls -la
+echo "Starting Flask app..."
+exec gunicorn main:app --bind 0.0.0.0:$PORT --workers 3
