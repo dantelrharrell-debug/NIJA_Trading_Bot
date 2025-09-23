505478b3fa9dbe96b1589488d1db5401bc848923
Dante <dantelrharrell@gmail.com>
2025-09-22 21:54:51 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
+cat > start.sh <<'SH'
 #!/bin/bash
-
-# Use Render's $PORT if defined, otherwise default to 5000 for local testing
 PORT_NUMBER=${PORT:-5000}
-
-# Start Gunicorn with your Flask app
 exec gunicorn main:app --bind 0.0.0.0:$PORT_NUMBER --workers 3
+SH
+
+chmod +x start.sh
