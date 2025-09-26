ecfe350e726695077113a642e28a22c465fc10a9
Dante <dantelrharrell@gmail.com>
2025-09-25 19:40:13 -0700

    Create signals.py

---
+++ signals.py
@@ -0,0 +1,10 @@
+# signals.py
+def get_nija_signals():
+    return {
+        "BTC": "long",
+        "ETH": "long",
+        "SOL": "short",
+        "XRP": "short",
+        "ADA": "flat",
+        "LTC": "flat"
+    }
