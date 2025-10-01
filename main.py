83da6b08dd4a571a1789be1c087884d366d8f140
Dante <dantelrharrell@gmail.com>
2025-09-30 17:12:29 -0700

    Update main.py

--- main.py
+++ main.py
@@ -138,3 +138,7 @@ def root():
 # ================================
 if __name__ == '__main__':
     app.run(port=5000)
+    if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port
+    app.run(host="0.0.0.0", port=port)
+
