c5d13d9fb768406c7b520f4a5cfe250a1dc10769
Dante <dantelrharrell@gmail.com>
2025-10-03 09:52:45 -0700

    Update start_debug.sh

--- start_debug.sh
+++ start_debug.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Starting NIJA Trading Bot..."
-# Run uvicorn and log all output to a file
-uvicorn main:app --host 0.0.0.0 --port ${PORT:-8000} &> debug.log
+# Run uvicorn and log everything to a file
+uvicorn main:app --host 0.0.0.0 --port ${PORT:-8000} &> debug.log || true
 echo "Uvicorn exited. Last 50 lines of debug.log:"
 tail -n 50 debug.log
