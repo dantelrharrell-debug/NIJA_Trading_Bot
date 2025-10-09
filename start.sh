b7bfb7da570b513f06b7c1cadd269ea7251ac80f
Dante <dantelrharrell@gmail.com>
2025-10-09 05:41:56 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
+# Upgrade pip first
+python3 -m pip install --upgrade pip
 
-# Exit immediately if a command exits with a non-zero status
-set -e
-
-# Activate virtual environment if it exists
-if [ -d "./venv" ]; then
-    source ./venv/bin/activate
-else
-    # Create virtual environment if missing
-    python3 -m venv venv
-    source ./venv/bin/activate
-fi
-
-# Upgrade pip to latest version
-pip install --upgrade pip
-
-# Install required dependencies
-pip install -r requirements.txt
+# Install all dependencies
+python3 -m pip install -r requirements.txt
 
 # Run the bot
-python3 main.py
+python3 -u nija_bot.py
