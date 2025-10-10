c954519deafde4ac02f91d7e9388420ff5535d7f
Dante <dantelrharrell@gmail.com>
2025-10-09 21:14:55 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-# Upgrade pip, setuptools, and wheel in the venv
+# Upgrade pip, setuptools, and wheel
 python3 -m pip install --upgrade pip setuptools wheel
 
-# Install dependencies from requirements.txt
+# Install dependencies
 python3 -m pip install -r requirements.txt
 
 # Run your bot
