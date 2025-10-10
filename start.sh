54f49f6f9329e4732536c3e5eda605a4b9a60a35
Dante <dantelrharrell@gmail.com>
2025-10-09 21:41:29 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
-# Upgrade pip and install requirements in Render's environment
+# Upgrade pip/wheel/setuptools
 python3 -m pip install --upgrade pip setuptools wheel
+
+# Install dependencies
 python3 -m pip install -r requirements.txt
 
 # Run the bot
