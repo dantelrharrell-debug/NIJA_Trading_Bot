08cdcdba16007c0c778a0fd03a8498bd2f328be3
Dante <dantelrharrell@gmail.com>
2025-10-09 21:38:50 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# Upgrade pip and install all dependencies
+# Upgrade pip and install requirements in Render's environment
 python3 -m pip install --upgrade pip setuptools wheel
 python3 -m pip install -r requirements.txt
 
