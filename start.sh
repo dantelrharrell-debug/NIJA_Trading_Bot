9aaa0a66a857e16dd50a53ff447e09ced59cb66c
Dante <dantelrharrell@gmail.com>
2025-10-09 22:16:28 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
 
-# Upgrade pip/wheel/setuptools
+# Upgrade pip safely in the current environment
 python3 -m pip install --upgrade pip setuptools wheel
 
-# Install dependencies
-python3 -m pip install -r requirements.txt
+# Force install the Coinbase package (in Render’s environment)
+python3 -m pip install --no-cache-dir coinbase-advanced-py==1.8.2
+
+# Install any other dependencies
+python3 -m pip install --no-cache-dir -r requirements.txt
 
 # Run the bot
 exec python3 nija_bot.py
