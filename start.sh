eb42052e86a537b9dda669fefd6c67625085f0b8
Dante <dantelrharrell@gmail.com>
2025-10-10 19:51:16 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,3 +7,8 @@ python3 -m pip install --no-cache-dir -r requirements.txt
 
 # Run bot
 python3 nija_bot.py
+
+#!/bin/bash
+pip install --upgrade pip
+pip install --force-reinstall coinbase-advanced-py==1.8.2
+pip install -r requirements.txt
