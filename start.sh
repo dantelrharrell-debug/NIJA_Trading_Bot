d442d3907588df314e77e1b8a35958f329eef5ce
Dante <dantelrharrell@gmail.com>
2025-10-10 20:00:15 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+set -e
+
+# Upgrade pip
+python3 -m pip install --upgrade pip
+
+# Install coinbase-advanced-py
+python3 -m pip install --force-reinstall coinbase-advanced-py==1.8.2
+
+# Install remaining requirements
+python3 -m pip install -r requirements.txt
+#!/bin/bash
 # exit immediately if a command exits with a non-zero status
 set -e
 
