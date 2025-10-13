43d93671525f169c6f6c00aff7b47a82134c251f
Dante <dantelrharrell@gmail.com>
2025-10-13 15:35:29 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
-
-# 1. Create virtual environment
 python3 -m venv .venv
 .venv/bin/python -m ensurepip --upgrade
 .venv/bin/pip install --upgrade pip
-
-# 2. Install requirements
 .venv/bin/pip install -r requirements.txt
-
-# 3. Start the bot
 .venv/bin/python main.py
