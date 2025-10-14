3af03b2ac95bfc8ec1d9f5b54d305250ed98f720
Dante <dantelrharrell@gmail.com>
2025-10-14 12:40:34 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-source .venv/bin/activate
-python3 nija_bot.py
 
-#!/bin/bash
+# Explicitly activate the virtual environment
+source "$PWD/.venv/bin/activate"
 
-# Activate virtual environment
-source .venv/bin/activate
-
-# Upgrade pip just in case
+# Optional: upgrade pip just to be safe
 python3 -m pip install --upgrade pip
 
-# Install requirements (safe to run on each start)
+# Ensure all packages from requirements.txt are installed
 python3 -m pip install -r requirements.txt
 
 # Run the bot
